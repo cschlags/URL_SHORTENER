28832113d613c4a293765da3662897fca9ca7ade
Christina Leuci <christinaleuci@gmail.com>
2015-09-29 14:02:00 -0400

    rspec

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,16 +1,35 @@
 
 class Url < ActiveRecord::Base
-  BASE_URL = "short-and-sweet.herokuapp.com/"
+  BASE_URL = "short-and-sweet.herokuapp.com"
   # each of these must be present
   validates :long_url, :presence => true
-  before_create :validate_original
+  before_create :validate_long
   after_create :generate_short
-  
+
+  validates :long_url, format: {
+    with: /\S{2}\.\S{1}/,
+    message: "Not a valid URL"
+  }
   # if user does not have http or in future when calling http
-  def validate_original
-    if !long_url.match(/^http/)
-      self.long_url = "http://#{long_url}"
+  def validate_long
+    # test to sanitize to remove slash at the end
+    includes_https = false
+    # Remove dangling '/'
+    self.long_url = self.long_url[0..-2] if self.long_url.last == '/' 
+    # Remove 'http://'
+    self.long_url = self.long_url[7..-1] if self.long_url[0..6] == 'http://'
+    # Remove 'https://'
+    if self.long_url[0..7] == 'https://'
+      self.long_url = self.long_url[8..-1]
+      includes_https = true
     end
+    # Remove 'www'
+    self.long_url = self.long_url[4..-1] if self.long_url[0..2] == 'www'
+    # Ensure the original url has 'http' or 'https' at the start
+    includes_https == true ? 
+      self.long_url = "https://#{self.long_url}" :
+      self.long_url = "http://#{self.long_url}"
+    self.long_url
   end
 
   # generate the short url
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -5,12 +5,10 @@ describe 'Urls' do
   context 'submitting a valid non-https url' do
     before :each do 
       @long_url = 'test.com'
-      @url = Url.new
-      @url.long_url = @long_url
-      @url.setup_short_url(@url)
+      @url = Url.create(long_url: @long_url)
     end
 
-    it 'creates a new site entry' do 
+    it 'creates a new site entry' do
       expect(Url.last.long_url.include?(@long_url)).to eq(true)
     end
 
@@ -19,47 +17,13 @@ describe 'Urls' do
     end
   end
 
-  context 'submitting a url that has already been submitted' do 
-    before :each do 
-      @long_url = 'http://test.com'
-      @url_first = Url.new
-      @url_first.long_url = @long_url
-      @url_first.setup_short_url(@url_first)
-
-      @url_2 = 'www.test.com'
-      @url_second = Url.new
-      @url_second.long_url = @url_2
-      @url_second.setup_short_url(@url_second)
-    end
-
-    it 'retrieves the existing site entry' do
-      expect(Url.find_by(long_url: @long_url))
-      
-      urls = []
-      Url.all.each do |site|
-        urls << site if Url.long_url.include?('test.com')
-      end
-
-      expect(urls[0].long_url).to eq('http://test.com')
-    end
-
-    it 'does not create a new site entry' do 
-      expect(Url.all.length).to eq(1)
-      expect(@url_second.id).to eq(nil)
-    end
-  end
-
   context 'submitting a valid https url' do
     before :each do 
-      @http_url = 'http://test2.com'
-      @http_site = Url.new
-      @http_url.long_url = @http_url
-      @http_url.setup_short_url(@http_site)
+      @long_url = 'http://test2.com'
+      @http_site = Url.create(long_url: @long_url)
       
-      @https_url = 'https://test2.com'
-      @https_site = Url.new
-      @https_url.long_url = @https_url
-      @https_url.setup_short_url(@https_site)
+      @long_url = 'https://test2.com'
+      @https_site = Url.create(long_url: @long_url)
     end
 
     it 'creates a new separate site entry' do
@@ -73,9 +37,7 @@ describe 'Urls' do
   context 'submitting an invalid url' do
     before :each do 
       @long_url = 'gibberish'
-      @url = Url.new
-      @url.long_url = @long_url
-      @url.setup_short_url(@url)
+      @url = Url.create(long_url: @long_url)
     end
 
     it 'does not create a new site entry' do 
