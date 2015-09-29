2e1a55dc56fc588019bdab5a9ceef7a4a783dd8c
Christina Leuci <christinaleuci@gmail.com>
2015-09-29 13:07:59 -0400

    x-accel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
