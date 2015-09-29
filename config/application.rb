bb1289058018658c73300800ed5dd281343d891b
Christina Leuci <christinaleuci@gmail.com>
2015-09-29 12:32:44 -0400

    heroku assets config assets true

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module URLShortener
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    config.assets.compile = true
   end
 end
