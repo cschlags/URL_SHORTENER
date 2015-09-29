c91e62735877b3af8225dce05ec0bbe338f16d32
Christina Leuci <christinaleuci@gmail.com>
2015-09-29 01:53:59 -0400

    added validation to model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,9 @@
 class Url < ActiveRecord::Base
+  # each of these must be present
+  validates :long_url, :short_url, :http_status, :presence => true
+  # the long_url must be unique, don't want multiples
+  validates :long_url, :uniqueness => true
+  
   # generate the short url
   def generate_short
     # the short_url is equal to the id to base 36
